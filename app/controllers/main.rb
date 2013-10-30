d4b6295807219712c87e61fe4b5ade7a42c34627
Theo Vora <theevo@gmail.com>
2013-10-30 11:52:44 -0400

    sinatra shows all public repos. todo: private repos

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -1,5 +1,12 @@
 class MainApplication < Sinatra::Base
+  set :views, File.expand_path('../../views', __FILE__)
+
   get '/' do
-    "greetings, earthling"
+    client = Octokit::Client.new \
+      :login => MY_LOGIN,
+      :password => MY_PASSWORD
+
+    @user = client.user
+    erb :'index'
   end
 end
